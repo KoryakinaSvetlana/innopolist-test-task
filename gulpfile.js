e5af4142592662e3276c8a44319d3c14fa0fea88
KoryakinaSvetlana <switch_sv@mail.ru>
2018-05-03 01:11:25 +0500

    Подключение скриптов. Настройка файла сборки проекта

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,96 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minifycss = require("gulp-csso");
+var minifyjs = require("gulp-uglify");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var del = require("del");
+var run = require('run-sequence');
+var include = require("posthtml-include");
+var posthtml = require("gulp-posthtml");
+var htmlmin = require('gulp-htmlmin');
 
-gulp.task("style", function() {
+/*Сбор и минимизация стилей*/
+gulp.task("style", function () {
   gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(minifycss())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+/*Минимизация js*/
+gulp.task('js', function () {
+  gulp.src(['source/scripts/*.js', '!source/scripts/*.min.js'])
+    .pipe(plumber())
+    .pipe(minifyjs())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('build/scripts'));
+});
+
+/*SVG Sprite*/
+gulp.task("sprite", function () {
+  return gulp.src(["source/img/sign-icon.svg",
+      "source/img/check-icon.svg",
+      "source/img/encode-icon.svg",
+      "source/img/decode-icon.svg"])
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+/*PostHtml*/
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest("build"));
+});
+
+/*Copy*/
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/scripts/**"
+      ], {
+      base: "source"
+      })
+    .pipe(gulp.dest("build"));
+});
+
+/*Clean*/
+gulp.task("clean", function () {
+  return del("build");
+});
+
+/*Build*/
+gulp.task("build", function (done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "js",
+    "sprite",
+    "html",
+    done
+  );
+});
+
+/*Watch*/
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+    server: "build/"
   });
 
   gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", ["html"]);
 });
--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,35 +1,242 @@
+var preloadFiles = [
+  {name: 'Текстовый документ.txt', size: 29, type: 'text/plain', lastModifiedDate: new Date(2017, 9, 13)},
+  {name: 'Текстовый документ.doc', size: 11438, type: 'application/msword', lastModifiedDate: new Date(2017, 9, 13)},
+  {name: 'Изображение.jpg', size: 4178, type: 'image/jpeg', lastModifiedDate: new Date(2017, 9, 13)},
+  {name: 'Гипертекстовая страница.html', size: 46, type: 'text/html', lastModifiedDate: new Date(2017, 9, 13)},
+];
+
+var uploadFiles = [];
+
+function getClassName(type) {
+  var result = 'file--txt';
+  switch (type) {
+    case 'text/plain':
+      result = 'file--txt';
+    break;
+    case 'application/msword':
+      result = 'file--doc';
+    break;
+    case 'image/jpeg':
+      result = 'file--jpg';
+    break;
+    case 'text/html':
+      result = 'file--html';
+    break;
+  }
+  return result;
+}
+
+function getSize(size) {
+  return (size < 1024) ? size + ' байт' :  Math.round(size / 1024 * 100) / 100 + ' Кб';
+}
+
+function getDate(date) {
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  return (day < 10 ? '0' + day : day) + '.' + (month < 10 ? '0' + month : month) + '.' + year;
+}
+
+function addTableRow(table, data, prefix) {
+  var count = table.find('tr').length;
+  var id = prefix + count;
+
+  var newRow = $("<tr>");
+  var cols = "";
+
+  cols += '<td class="checkbox"><input type="checkbox" id="' + id + '"><label for="' + id + '"></label></td>';
+  cols += '<td><span class="file">' + data.name + '</span><span>' + getSize(data.size) + '</span></td>';
+  cols += '<td>от '+ getDate(data.lastModifiedDate) +'</td>';
+
+  newRow.append(cols);
+  //add Class
+  newRow.find('.file').addClass(getClassName(data.type));
+  //add event listener
+  newRow.find('input[type="checkbox"]').on('click', checkboxClick);
+  //add row to table
+  table.append(newRow);
+}
+
+function clearTable(table) {
+  table.find("tr").remove();
+}
+
+function checkboxClick(e) {
+  var id = $(this).prop('id');
+  //uncheck other checknox
+  $('.file-actions__table input[type="checkbox"]').each(function() {
+    if ($(this).prop('id') != id) {
+      $(this).prop('checked', false);
+      $(this).parent().parent().removeClass('file-actions__checked-item');
+    }
+  });
+
+  if ($(this).prop('checked')) {
+    $(this).parent().parent().addClass('file-actions__checked-item');
+  } else {
+    $(this).parent().parent().removeClass('file-actions__checked-item');
+  }
+
+  if ($('.file-actions__table input[type="checkbox"]:checked').length > 0) {
+    showStep(2);
+  } else {
+    hideStep();
+  }
+}
+
+function showStep(num) {
+  $('.steps__item:eq(' + (num - 1) + ')').removeClass('steps__item--hidden');
+}
+
+function hideStep() {
+  $('.steps__item:gt(0)').addClass('steps__item--hidden');
+  $('#apply').prop("disabled", true);
+  clearOperation();
+}
+
+function clearOperation() {
+  $('.steps__operation input[type="radio"]').prop('checked', false);
+}
+
+function getSelectedFile() {
+  var index;
+  var result = false;
+  var checkElem = $('.file-actions__table input[type="checkbox"]:checked');
+  if (checkElem.length > 0) {
+     if (checkElem.prop("id").indexOf('upload')!== -1) {
+       index = checkElem.prop("id").substring(6);
+       result = uploadFiles[index];
+     } else {
+       index = checkElem.prop("id").substring(7);
+       result = preloadFiles[index];
+     }
+  }
+  return result;
+}
+
 $(function() {
+  var $form = $('.box');
+  var $toggleButton = $('.navigation__toggle button');
+  var $navigation = $('.navigation');
+  var $input = $form.find('input[type="file"]');
+  var $menuItem = $('.navigation__popup-item a');
+  var $fileCheck = $('.file-actions__table input[type="checkbox"]');
+  var $operations = $('.steps__operation input[type="radio"]');
+  var $applyButton = $('#apply');
+  var $modal = $('.modal');
+  var $modalCloseButton = $('.modal-close');
+  var $newStartButton = $('#newStart');
+
+  //init page
+  var init = function() {
     $('.file-actions__table-wrapper').simplebar();
 
-    var isAdvancedUpload = function() {
-      var div = document.createElement('div');
-      return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
-    }();
+    //data for preload table
+    clearTable($('#preloadFilesTable'));
 
-    var $form = $('.box');
+    $.each(preloadFiles, function( index, item ) {
+      addTableRow($('#preloadFilesTable'), item, 'preload')
+    });
 
-    if (isAdvancedUpload) {
-      $form.addClass('has-advanced-upload');
-      var droppedFiles = false;
+    clearTable($('#uploadFilesTable'));
+  }();
 
-      $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-      })
-      .on('dragover dragenter', function() {
-        $form.addClass('is-dragover');
-      //  console.log('dragover dragenter');
-      })
-      .on('dragleave dragend drop', function() {
-        $form.removeClass('is-dragover');
-        console.log('dragleave dragend drop');
-      })
-      .on('drop', function(e) {
-        droppedFiles = e.originalEvent.dataTransfer.files;
-        console.log('drop');
-        console.log(droppedFiles);
+  //check browser abilities
+  var isAdvancedUpload = function() {
+    var div = document.createElement('div');
+    return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+  }();
+
+  //events
+  $toggleButton.on('click', function(e) {
+    e.preventDefault();
+    $navigation.toggleClass('navigation--open');
+  });
+
+  $menuItem.on('click', function(e) {
+    e.preventDefault();
+    $navigation.removeClass('navigation--open');
+  });
+
+  $fileCheck.on('click', checkboxClick);
+
+  $operations.on('click', function(e) {
+    showStep(3);
+    $applyButton.prop("disabled", false);
+  });
+
+  $applyButton.on('click', function(e) {
+    $('.modal__content p').remove();
+
+    var file = getSelectedFile();
+    var fileInfo = $("<p>");
+    var fileInfoContent = '<strong>Выбранный файл:</strong> ' + (file ? file.name : '-');
+    fileInfo.append(fileInfoContent);
+
+    var operationInfo = $("<p>");
+    var operationInfoContent = '<strong>Операция:</strong> ' + $('.steps__operation input[type="radio"]:checked').prop('id');
+    operationInfo.append(operationInfoContent);
+
+    var signTypeInfo = $("<p>");
+    var signTypeInfoContent = '<strong>Тип подписи:</strong> ' + ($('#sign-type').prop('checked') ? 'Отсоединенная' : 'Присоединенная');
+    signTypeInfo.append(signTypeInfoContent);
+
+    $('.modal__content').append(fileInfo);
+    $('.modal__content').append(operationInfo);
+    $('.modal__content').append(signTypeInfo);
+
+    $modal.addClass('modal--open');
+  });
+
+  $modalCloseButton.on('click', function(e) {
+    $modal.removeClass('modal--open');
+  });
+
+  $newStartButton.on('click', function(e) {
+    $modal.removeClass('modal--open');
+
+    $('.file-actions__table input[type="checkbox"]').each(function() {
+      $(this).prop('checked', false);
+      $(this).parent().parent().removeClass('file-actions__checked-item');
+    });
+
+    uploadFiles = [];
+    $('#uploadFilesTable tr').remove();
+    $('#uploadFilesTableContainer').addClass('file-actions__table-wrapper--hidden');
+    hideStep();
+  });
+
+  //File Uploader
+  if (isAdvancedUpload) {
+    $form.addClass('has-advanced-upload');
+    var droppedFiles = false;
+
+    $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    })
+    .on('dragover dragenter', function() {
+      $form.addClass('is-dragover');
+    })
+    .on('dragleave dragend drop', function() {
+      $form.removeClass('is-dragover');
+    })
+    .on('drop', function(e) {
+      droppedFiles = e.originalEvent.dataTransfer.files;
+      $('#uploadFilesTableContainer').removeClass('file-actions__table-wrapper--hidden');
+      $.each( droppedFiles, function(i, file) {
+        addTableRow($('#uploadFilesTable'), file, 'upload');
+        uploadFiles.push(file)
       });
+    });
+  }
 
-    }
+  $input.on('change', function(e) {
+    $('#uploadFilesTableContainer').removeClass('file-actions__table-wrapper--hidden');
+    $.each( e.target.files, function(i, file) {
+      addTableRow($('#uploadFilesTable'), file, 'upload');
+      uploadFiles.push(file)
+    });
+  });
 
 });
